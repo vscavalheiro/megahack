9ef4871165f7b7a1575e3c01bae6bb9b96443736
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-04-29 23:04:06 -0300

    Exemplo src

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Fragment} from 'react';
+
+import Login from './src/pages/Login'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Fragment>
+      <Login /> 
+    </Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
