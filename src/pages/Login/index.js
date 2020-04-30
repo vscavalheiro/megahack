4fff933cdebce0242f636025288904879bba530e
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-04-29 23:04:06 -0300

    Exemplo src

---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Texto from '../../components/Texto'
+import { View } from 'react-native';
+import styles from './styles';
+
+
+const Login = () => {
+    return (
+        <View style={styles.container}>
+            <Texto />
+        </View>
+    )
+}
+
+
+export default Login;
---
+++ src/themes/colors.js
@@ -0,0 +1,16 @@
+const COLORS = {
+
+    BLUE_6: '#23233bff',
+    BLUE_5: '#2c4268ff',
+    BLUE_4: '#007bbaff',
+    BLUE_3: '#00a9e2ff',
+    BLUE_2: '#7ccdf4ff',
+    BLUE_1: '#bce3faff',
+    GREY_4: '#9b9c9bff',
+    GREY_3: '#b2b0b0ff',
+    GREY_2: '#c5c6c6ff',
+    GREY_1: '#ebebebff',
+
+}
+  
+  export default COLORS;
