cfd75442e22d3f15b950ccd07ea71f6a39e88af8
João <jvgewehr@gmail.com>
2020-05-02 18:08:51 -0300

    Add ItemGasto clicado

---
+++ src/components/ItemGasto/AvaliacaoTarefa/avaliacaoTarefa.js
@@ -0,0 +1,13 @@
+import React, {Fragment, useState} from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+
+const AvaliacaoTarefa = props => {
+    return(
+        <View>
+            <Text>Foi clicado</Text>
+        </View>
+    );
+}
+export default AvaliacaoTarefa
